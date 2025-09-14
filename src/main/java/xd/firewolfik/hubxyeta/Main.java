cc99efcb666d4173c0e2212d8570aa88f735726b
firewolfik <timofeigvozdikov@gmail.com>
2025-09-14 20:35:13 +0300

    add watermark

--- src/main/java/xd/firewolfik/hubxyeta/Main.java
+++ src/main/java/xd/firewolfik/hubxyeta/Main.java
@@ -1,6 +1,7 @@
 package xd.firewolfik.hubxyeta;
 
 import lombok.Getter;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,33 +24,37 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        // Сохраняем дефолтные конфиги
         saveDefaultConfig();
         saveResource("items.yml", false);
         saveResource("messages.yml", false);
 
-        // Загружаем конфиг сообщений
         loadMessagesConfig();
 
-        // Инициализируем менеджеры
         configManager = new ConfigManager(this);
         itemsManager = new ItemsManager(this);
 
-        getLogger().info("Загрузка HubXyEta...");
+        getLogger().info(ChatColor.YELLOW + "#############################");
+        getLogger().info(ChatColor.WHITE + "HubXYETA" + ChatColor.GRAY + " - " + ChatColor.GREEN + "включен");
+        getLogger().info(ChatColor.WHITE + "Автор:" + ChatColor.GOLD + " firewolfik");
+        getLogger().info(ChatColor.WHITE + "Связь с разработчиком:" + ChatColor.YELLOW + " t.me/oooSwagParty");
+        getLogger().info(ChatColor.WHITE + "Версия плагина:" + ChatColor.GOLD + " 2.1" + ChatColor.GRAY + " (Fix errors 14.09.25)");
+        getLogger().info(ChatColor.YELLOW + "#############################");
 
-        // Регистрируем слушатели
         getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
 
-        // Регистрируем команды
         registerCommands();
 
-        getLogger().info("HubXyEta успешно загружен!");
-        getLogger().info("Загружено предметов: " + itemsManager.getAllItems().size());
+        getLogger().info(ChatColor.GRAY +"[Info]" + ChatColor.WHITE + " Загружено предметов: " + ChatColor.YELLOW + itemsManager.getAllItems().size());
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("HubXyEta выгружен!");
+        getLogger().info(ChatColor.YELLOW + "#############################");
+        getLogger().info(ChatColor.WHITE + "HubXYETA" + ChatColor.GRAY + " - " + ChatColor.RED + "выключен");
+        getLogger().info(ChatColor.WHITE + "Автор:" + ChatColor.GOLD + " firewolfik");
+        getLogger().info(ChatColor.WHITE + "Связь с разработчиком:" + ChatColor.YELLOW + " t.me/oooSwagParty");
+        getLogger().info(ChatColor.WHITE + "Версия плагина:" + ChatColor.GOLD + " 2.1" + ChatColor.GRAY + " (Fix errors 14.09.25)");
+        getLogger().info(ChatColor.YELLOW + "#############################");
     }
 
     private void registerCommands() {
@@ -75,7 +80,7 @@ public final class Main extends JavaPlugin {
         configManager.reloadConfigs();
         itemsManager.reloadItems();
         playerListener.restartAllActionBars();
-        getLogger().info("Плагин перезагружен!");
+        getLogger().info(ChatColor.WHITE +"[" + ChatColor.YELLOW + "Info" + ChatColor.WHITE + "]" + ChatColor.WHITE + " Плагин " + ChatColor.GREEN + "перезагружен");
     }
     public FileConfiguration getMessagesConfig() {
         return messagesConfig;
